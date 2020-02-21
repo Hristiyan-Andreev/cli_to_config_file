f9d93acda187d8e265af3358d3f1e4183ca0ed00
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-02-21 13:00:00 +0200

    Autoreload on file change via watchdog.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-perkele = 1
+import json
+
+config_file = 'config.json'
+
+with open(config_file) as cf_file:
+    config_dict = json.load(cf_file)
+
+
+perkele = config_dict['perkele']
+download_csv = config_dict['download_csv']
+data = config_dict['data']
--- watchdog.py
+++ watchdog.py
@@ -3,26 +3,29 @@
 import os
 import sys
 from os.path import getmtime
+import threading as th
+import time
 
 import config as cf
 
-# Parse script arguments and configuration files.
-# ...
-
-WATCHED_FILES = ['config.py']
+WATCHED_FILES = ['config.json']
 WATCHED_FILES_MTIMES = [(f, getmtime(f)) for f in WATCHED_FILES]
-print(cf.perkele)
+print('\n{}'.format(cf.perkele))
 
-while True:
-    # Wait for inputs and act on them.
 
-    # Check whether a watched file has changed.
-    for f, mtime in WATCHED_FILES_MTIMES:
-        if getmtime(f) != mtime:
-            # One of the files has changed, so restart the script.
-            print('--> restarting')
-            # When running the script via `./daemon.py` (e.g. Linux/Mac OS), use
-            # os.execv(__file__, sys.argv)
-            # When running the script via `python daemon.py` (e.g. Windows), use
-            os.execv(sys.executable, ['python'] + sys.argv)
+def watch_reload_func():
 
+    while True:
+        time.sleep(5)
+        # Check whether a watched file has changed.
+        for f, mtime in WATCHED_FILES_MTIMES:
+            if getmtime(f) != mtime:
+                # One of the files has changed, so restart the script.
+                print('--> restarting')
+                # When running the script via `./daemon.py` (e.g. Linux/Mac OS), use
+                # os.execv(__file__, sys.argv)
+                # When running the script via `python daemon.py` (e.g. Windows), use
+                os.execv(sys.executable, ['python'] + sys.argv)
+
+reload_thread = th.Thread(target=watch_reload_func)
+reload_thread.start()
