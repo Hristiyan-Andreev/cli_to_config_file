23e462d8fa52f8128672d40fc22a09c2aaa49047
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-02-21 15:03:54 +0200

    Watchdog class working as thread subclass

--- watchdog.py
+++ watchdog.py
@@ -1,35 +1,28 @@
 # !/usr/bin/env python3
 
 import os
-import sys
-from os.path import getmtime
-import threading as th
+# import sys
+# from os.path import getmtime
+# import threading as th
 import time
+import watchdog_class as wtd
 
 import config as cf
 
-WATCHED_FILES = ['config.json']
-WATCHED_FILES_MTIMES = [(f, getmtime(f)) for f in WATCHED_FILES]
-print('\n{}'.format(cf.perkele))
 
+def main():
+    WATCHED_FILES = ['config.json']
 
-def watch_reload_func():
+    reload_thread = wtd.WatchDogReload(WATCHED_FILES, linux=False)
+    reload_thread.start()
 
-    while True:
+    while(True):
+        print(cf.config_dict)
         time.sleep(5)
-        # Check whether a watched file has changed.
-        for f, mtime in WATCHED_FILES_MTIMES:
-            if getmtime(f) != mtime:
-                # One of the files has changed, so restart the script.
-                print('--> restarting')
-                # When running the script via `./daemon.py` (e.g. Linux/Mac OS), use
-                # os.execv(__file__, sys.argv)
-                # When running the script via `python daemon.py` (e.g. Windows), use
-                os.execv(sys.executable, ['python'] + sys.argv)
 
-reload_thread = th.Thread(target=watch_reload_func)
-reload_thread.start()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Bye bye')
 
-import watchdog_class as wtd
-
-wtd.WatchDogReload()
--- watchdog_class.py
+++ watchdog_class.py
@@ -4,29 +4,34 @@ from os.path import getmtime
 import threading as th
 import time
 
-class WatchDogReload():
-    def __init__(self, files_to_watch, check_interval = 5, linux = True):
+class WatchDogReload(th.Thread):
+    def __init__(self, files_to_watch, check_interval = 2, linux = True):
         '''
         @files_to_watch: Files watched for changes list of strings - e.g.\
              ['config.json','file.py']
         @check_interval: Time in seconds in between file change checks
         @linux: True if called on linux, false if called on windows
         '''
+
+        th.Thread.__init__(self, name='watch_and_reload_thread', daemon=True)
         self.files = files_to_watch
         self.start_up_edit_times = [(f, getmtime(f)) for f in self.files]
         self.check_interval = check_interval
-        self.daemon = daemon
+        self.linux = linux
 
-    def watch_and_reload(self):
+    def run(self):
         while True:
             time.sleep(self.check_interval)
             # Check whether a watched file has changed.
-            for f, mtime in self.files:
+            for f, mtime in self.start_up_edit_times:
                 if getmtime(f) != mtime:
                     # One of the files has changed, so restart the script.
                     print('--> restarting')
+                    if self.linux:
                     # When running the script via `./daemon.py` (e.g. Linux/Mac OS), use
-                    # os.execv(__file__, sys.argv)
+                        # os.execv(__file__, sys.argv)
+                        pass
+                    elif not self.linux:
                     # When running the script via `python daemon.py` (e.g. Windows), use
-                    os.execv(sys.executable, ['python'] + sys.argv)
+                        os.execv(sys.executable, ['python'] + sys.argv)
         
