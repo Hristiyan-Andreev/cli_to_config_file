8ef4c85b8ff2d87b2a794070cf05dbb637845c46
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-02-24 16:43:14 +0200

    Save config to JSON added

--- config_manager.py
+++ config_manager.py
@@ -3,10 +3,15 @@ import sys
 import json
 
 class ConfigManager():
-    def __init__(self):
-        self.DEFAULT_CONFIG = self.load_default_cfg('default_config.json')
-        self.CURRENT_CONFIG = self.load_current_cfg('config.json')
-        self.NEW_CONFIG = self.load_current_cfg('config.json')
+    def __init__(self, cfg_file, default_cfg_file):
+        # default_config.json
+        # config.json
+        self.cfg_file = cfg_file
+        self.default_cfg_file = default_cfg_file
+
+        self.DEFAULT_CONFIG = self.load_default_cfg(default_cfg_file)
+        self.CURRENT_CONFIG = self.load_current_cfg(cfg_file)
+        self.NEW_CONFIG = self.load_current_cfg(cfg_file)
 
 
     def load_default_cfg(self, default_cfg_file):
@@ -20,6 +25,7 @@ class ConfigManager():
             current_config = json.load(current_cfg_file)
         return current_config
 
+
     def preview_changes(self):
         print('Changes:')
         for key, value in self.CURRENT_CONFIG.items():
@@ -27,9 +33,15 @@ class ConfigManager():
                 print('{}: {} --> {}'.format(key, value, self.NEW_CONFIG[key]))
 
 
-cfg = ConfigManager()
+    def save_config(self):
+        with open(self.cfg_file, 'w') as outfile:
+            json.dump(self.NEW_CONFIG, outfile)
+
+
+cfg = ConfigManager('config.json', 'default_config.json')
 cfg.NEW_CONFIG['elemental_ip'] = '192.168.2.15'
 cfg.NEW_CONFIG['elemental_pass'] = 'pass'
 
 cfg.preview_changes()
+cfg.save_config()
 
