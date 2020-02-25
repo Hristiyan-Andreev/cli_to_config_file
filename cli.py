d299984a62990dee91a9aa17fe28747b2900643b
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-02-25 15:09:39 +0200

    Main menu + config options added
    Config_manager class integrated in the CLI

--- cli.py
+++ cli.py
@@ -6,36 +6,27 @@ import json
 import validators as val
 from config_manager import ConfigManager
 
+
+# Used universally through the program as "back one step" flag
 EXIT_FLAG = 123
 
-# # Load default config file
-# default_config = 'default_config.json'
-# with open(default_config) as df_config_file:
-#     DEFAULT_CONFIG = json.load(df_config_file)
-
-# print(DEFAULT_CONFIG)
-
-# # Load current config file
-# current_config = 'config.json'
-# with open(current_config) as current_cfg:
-#     CURRENT_CONFIG = json.load(current_cfg)
-
-# print(CURRENT_CONFIG)
-
-# # Dictionary to save config parameters before dumping to JSON
-# NEW_CONFIG = CURRENT_CONFIG
-
-cfg = ConfigManager()
-cfg.preview_changes()
-
+# Used to manage configuration files - read, write and changes
+cfg = ConfigManager('config.json', 'default_config.json')
 
 # Dicts of menu options - to avoid using strings in the whole program
 mm_choices = {
+    'current_sett': 'Show current settings from file',
+    'edit_sett':'Edit settings',
+    'preview_ch': 'Preview changes',
+    'save_ch': 'Save changes to file',
+    'back': 'Exit the program'
+}
+
+main_cfg_choices = {
     'elemental':'Elemental server - IP and credentials',
     'stream': 'Stream-GPI pairs - number of streams and GPI mapping',
     'avail': 'Minimum avail duration - Enable/Disable, Duration',
-    'back': 'Exit'
-
+    'back': 'Back to main menu'
 }
 
 av_choices = {
@@ -51,174 +42,220 @@ el_choices = {
 }
 
 
-def elemental_menu():
-    elemental_promt = [
-        {
+def main_menu():
+    main_menu_promt = {
+        'type': 'list',
+        'name': 'main_choice',
+        'message': 'Choose an option\n',
+        'choices': [mm_choices['current_sett'], mm_choices['edit_sett'],\
+                     mm_choices['preview_ch'], mm_choices['save_ch'],\
+                     mm_choices['back']]
+    }
+
+    save_promt = {
+        'type': 'confirm',
+        'name': 'save?',
+        'message': 'Are you sure you want to save these changes to the config file?',
+        'default': True
+    }
+
+    while(True):
+        mm_answers = pyq.prompt(main_menu_promt)
+
+        if mm_answers['main_choice'] is mm_choices['back']:
+            break
+
+        elif mm_answers['main_choice'] is mm_choices['current_sett']:
+            cfg.show_current_cfg()
+
+        elif mm_answers['main_choice'] is mm_choices['edit_sett']:
+            main_config_menu()
+
+        elif mm_answers['main_choice'] is mm_choices['preview_ch']:
+            cfg.preview_changes()
+
+        elif mm_answers['main_choice'] is mm_choices['save_ch']:
+            cfg.preview_changes()
+            save_answer = pyq.prompt(save_promt)
+            if save_answer['save?'] is True:
+                cfg.save_config()
+
+
+def main_config_menu():
+    while(True):
+        main_options_promt = {
             'type': 'list',
-            'name': 'elemental',
-            'message': 'Elemenal Live server options\n',
-            'choices': [el_choices['ip'], el_choices['credentials'], \
-                        el_choices['back']]
-        },
-        {
-            'type': 'input',
-            'name': 'elemental_ip',
-            'message': 'Enter the Elemenal Live server IP address - without port',
-            'validate': val.IpValidator,
-            'when': lambda elemental_answers: elemental_answers['elemental'] \
-                 is el_choices['ip'],
-            # 'filter': lambda val: float(val)
-        },
-        {
-            'type': 'input',
-            'name': 'elemental_user',
-            'message': 'Enter Elemental Live username',
-            'when': lambda elemental_answers: elemental_answers['elemental'] \
-                 is el_choices['credentials']
-        },
-        {
-            'type': 'password',
-            'name': 'elemental_pass',
-            'message': 'Enter Elemental Live password',
-            'when': lambda elemental_answers: elemental_answers['elemental'] \
-                 is el_choices['credentials']
+            'name': 'main_option',
+            'message': 'What you want to configure?\n',
+            'choices': [main_cfg_choices['elemental'], main_cfg_choices['stream'], \
+                        main_cfg_choices['avail'], main_cfg_choices['back']]
         }
-    ]
 
-    elemental_answers = pyq.prompt(elemental_promt)
-    if elemental_answers['elemental'] is el_choices['ip']:
-        NEW_CONFIG['elemental_ip'] = elemental_answers['elemental_ip']
+        answers = pyq.prompt(main_options_promt)
 
-    elif elemental_answers['elemental'] is el_choices['credentials']:
-        NEW_CONFIG['elemental_user'] = elemental_answers['elemental_user']
-        NEW_CONFIG['elemental_pass'] = elemental_answers['elemental_pass']
+        if answers['main_option'] is main_cfg_choices['back']:
+            break
 
-    elif elemental_answers['elemental'] is el_choices['back']:
-        print(NEW_CONFIG)
-        return EXIT_FLAG
+        elif answers['main_option'] is main_cfg_choices['elemental']:
+            elemental_menu()
 
-    else:
-        return 0
+        elif answers['main_option'] is main_cfg_choices['stream']:
+            stream_menu()
+
+        elif answers['main_option'] is main_cfg_choices['avail']:
+            avail_menu()
+
+
+def elemental_menu():
+    while(True):
+        elemental_promt = [
+            {
+                'type': 'list',
+                'name': 'elemental',
+                'message': 'Elemenal Live server options\n',
+                'choices': [el_choices['ip'], el_choices['credentials'], \
+                            el_choices['back']]
+            },
+            {
+                'type': 'input',
+                'name': 'elemental_ip',
+                'message': 'Enter the Elemenal Live server IP address - without port',
+                'validate': val.IpValidator,
+                'when': lambda elemental_answers: elemental_answers['elemental'] \
+                    is el_choices['ip'],
+                # 'filter': lambda val: float(val)
+            },
+            {
+                'type': 'input',
+                'name': 'elemental_user',
+                'message': 'Enter Elemental Live username',
+                'when': lambda elemental_answers: elemental_answers['elemental'] \
+                    is el_choices['credentials']
+            },
+            {
+                'type': 'password',
+                'name': 'elemental_pass',
+                'message': 'Enter Elemental Live password',
+                'when': lambda elemental_answers: elemental_answers['elemental'] \
+                    is el_choices['credentials']
+            }
+        ]
+
+        elemental_answers = pyq.prompt(elemental_promt)
+        if elemental_answers['elemental'] is el_choices['ip']:
+            cfg.NEW_CONFIG['elemental_ip'] = elemental_answers['elemental_ip']
+
+        elif elemental_answers['elemental'] is el_choices['credentials']:
+            cfg.NEW_CONFIG['elemental_user'] = elemental_answers['elemental_user']
+            cfg.NEW_CONFIG['elemental_pass'] = elemental_answers['elemental_pass']
+
+        elif elemental_answers['elemental'] is el_choices['back']:
+            print(cfg.NEW_CONFIG)
+            break
 
 
 def stream_menu():
-    stream_num_promt = [
-        {
-            'type': 'input',
-            'name': 'gpi_num',
-            'message': 'Enter the number of GPI-Stream pairs',
-            # 'validate':
-            # 'filter': lambda input: int(input)
-        }
-    ]
-    stream_answers = pyq.prompt(stream_num_promt)
-    num_of_streams = int(stream_answers['gpi_num'])
 
-    gpi_to_stream_map = {}
-    for stream in range(1, num_of_streams+1):
-        gpi_promt = [
+    while(True):
+        stream_num_promt = [
             {
                 'type': 'input',
-                'name': 'gpi_{}'.format(stream),
-                'message': 'Enter the GPI pin for GPI-Stream pair number ({}):'.format(stream),
-                # 'validate': lambda input: not isinstance(input, int) or 'Enter an int value',
+                'name': 'gpi_num',
+                'message': 'Enter the number of GPI-Stream pairs',
+                # 'validate':
                 # 'filter': lambda input: int(input)
             }
         ]
-        gpi_answer = pyq.prompt(gpi_promt)
-        gpi_pin = gpi_answer['gpi_{}'.format(stream)]
+        stream_answers = pyq.prompt(stream_num_promt)
+        num_of_streams = int(stream_answers['gpi_num'])
 
-        stream_promt = [
+
+        gpi_to_stream_map = {}
+        for stream in range(1, num_of_streams+1):
+            gpi_promt = [
+                {
+                    'type': 'input',
+                    'name': 'gpi_{}'.format(stream),
+                    'message': 'Enter the GPI pin for GPI-Stream pair number ({}):'.format(stream),
+                    # 'validate': lambda input: not isinstance(input, int) or 'Enter an int value',
+                    # 'filter': lambda input: int(input)
+                }
+            ]
+            gpi_answer = pyq.prompt(gpi_promt)
+            gpi_pin = gpi_answer['gpi_{}'.format(stream)]
+
+            stream_promt = [
+                {
+                    'type': 'input',
+                    'name': 'stream_{}'.format(stream),
+                    'message': 'Enter the Elemental event number for GPI ({}):'.format(gpi_pin),
+                }
+            ]
+            stream_answer = pyq.prompt(stream_promt)
+            event_number = stream_answer['stream_{}'.format(stream)]
+
+            gpi_to_stream_map[gpi_pin] = event_number
+        
+        print(gpi_to_stream_map)
+        done_with_streams_promt = [
             {
-                'type': 'input',
-                'name': 'stream_{}'.format(stream),
-                'message': 'Enter the Elemental event number for GPI ({}):'.format(gpi_pin),
+                'type': 'list',
+                'name': 'done?',
+                'message': 'Is the mapping correct?\n',
+                'choices': ['Yes, Save & Exit', 'No, edit the GPI-Stream pairs again']
             }
         ]
-        stream_answer = pyq.prompt(stream_promt)
-        event_number = stream_answer['stream_{}'.format(stream)]
-
-        gpi_to_stream_map[gpi_pin] = event_number
-    
-    print(gpi_to_stream_map)
-
-    # if stream_answers['back']: return EXIT_FLAG
-
-    return 0
-
+        done_answer = pyq.prompt(done_with_streams_promt)
+        
+        if done_answer['done?'] is 'Yes, Save & Exit':
+            cfg.NEW_CONFIG['gpi_to_stream_map'] = gpi_to_stream_map
+            break
+        
 
 def avail_menu():
-    avails_promt = [
-        {
-            'type': 'list',
-            'name': 'avail',
-            'message': 'Avail duration options',
-            'choices': [av_choices['enable'], av_choices['duration'], \
-                        av_choices['back']]
-        },
-        {
-            'type': 'list',
-            'name': 'enable',
-            'message': 'Do you want to enable minimum avail duration',
-            'choices': ['Yes', 'No'],
-            'when': lambda avails: avails['avail'] is av_choices['enable']
-        },
-        {
-            'type': 'input',
-            'name': 'avail_duration',
-            'message': 'Enter the minimum avail duration in seconds',
-            'validate': val.AvDurValidator,
-            'when': lambda avails: avails['avail'] is av_choices['duration'],
-            'filter': lambda val: float(val)
-        }
-    ]
+    while(True):
+        avails_promt = [
+            {
+                'type': 'list',
+                'name': 'avail',
+                'message': 'Avail duration options',
+                'choices': [av_choices['enable'], av_choices['duration'], \
+                            av_choices['back']]
+            },
+            {
+                'type': 'list',
+                'name': 'enable',
+                'message': 'Do you want to enable minimum avail duration',
+                'choices': ['Yes', 'No'],
+                'when': lambda avails: avails['avail'] is av_choices['enable']
+            },
+            {
+                'type': 'input',
+                'name': 'avail_duration',
+                'message': 'Enter the minimum avail duration in seconds',
+                'validate': val.AvDurValidator,
+                'when': lambda avails: avails['avail'] is av_choices['duration'],
+                'filter': lambda val: float(val)
+            }
+        ]
 
-    avails = pyq.prompt(avails_promt)
+        avails = pyq.prompt(avails_promt)
 
-    if avails['avail'] is av_choices['back']:
-        return EXIT_FLAG
-    else:
-        return 0
-   
+        if avails['avail'] is av_choices['enable']:
+            cfg.NEW_CONFIG['min_avail_enabled'] = True if avails['enable'] == 'Yes' else False
+            print(cfg.NEW_CONFIG)
 
-def main_menu():
-    main_options_promt = {
-        'type': 'list',
-        'name': 'main_option',
-        'message': 'What you want to configure?\n',
-        'choices': [mm_choices['elemental'], mm_choices['stream'], \
-                    mm_choices['avail'], mm_choices['back']]
-    }
+        elif avails['avail'] is av_choices['duration']:
+            cfg.NEW_CONFIG['min_avail_duration'] = avails['avail_duration']
+            print(cfg.NEW_CONFIG)
 
-    answers = pyq.prompt(main_options_promt)
-
-    if answers['main_option'] is mm_choices['back']:
-        return EXIT_FLAG
-    else:
-        return answers
-    
-
+        if avails['avail'] is av_choices['back']:
+            break
+        
+  
 def whole_menu():
 
-    while(True):
-        mm_answers = main_menu()
-
-
-        if mm_answers is EXIT_FLAG:
-            break
-
-        elif mm_answers['main_option'] is mm_choices['elemental']:
-            while(True):
-                if elemental_menu() is EXIT_FLAG: break                
-
-        elif mm_answers['main_option'] is mm_choices['stream']:
-            while(True):
-                if stream_menu() is EXIT_FLAG: break
-
-        elif mm_answers['main_option'] is mm_choices['avail']:
-            while(True):
-                if avail_menu() is EXIT_FLAG: break
-
+    main_menu()           
     return 0
 
 
--- config_manager.py
+++ config_manager.py
@@ -9,21 +9,31 @@ class ConfigManager():
         self.cfg_file = cfg_file
         self.default_cfg_file = default_cfg_file
 
-        self.DEFAULT_CONFIG = self.load_default_cfg(default_cfg_file)
-        self.CURRENT_CONFIG = self.load_current_cfg(cfg_file)
-        self.NEW_CONFIG = self.load_current_cfg(cfg_file)
+        self.load_default_cfg(self.cfg_file)
+        self.load_current_cfg(self.cfg_file)
+
+        self.NEW_CONFIG = self.CURRENT_CONFIG
 
 
     def load_default_cfg(self, default_cfg_file):
         with open(default_cfg_file) as df_cfg_file:
             default_config = json.load(df_cfg_file)
-        return default_config
+
+        self.DEFAULT_CONFIG = default_config
 
 
     def load_current_cfg(self, current_cfg_file):
         with open(current_cfg_file) as current_cfg_file:
             current_config = json.load(current_cfg_file)
-        return current_config
+        
+        self.CURRENT_CONFIG = current_config
+
+
+    def show_current_cfg(self):
+        self.load_current_cfg(self.cfg_file)
+        print('Current settings in config file: {}'.format(self.cfg_file))
+        for key, value in self.CURRENT_CONFIG.items():
+             print('{}: {}'.format(key, value))
 
 
     def preview_changes(self):
@@ -36,12 +46,5 @@ class ConfigManager():
     def save_config(self):
         with open(self.cfg_file, 'w') as outfile:
             json.dump(self.NEW_CONFIG, outfile)
-
-
-cfg = ConfigManager('config.json', 'default_config.json')
-cfg.NEW_CONFIG['elemental_ip'] = '192.168.2.15'
-cfg.NEW_CONFIG['elemental_pass'] = 'pass'
-
-cfg.preview_changes()
-cfg.save_config()
+        self.load_current_cfg(self.cfg_file)
 
